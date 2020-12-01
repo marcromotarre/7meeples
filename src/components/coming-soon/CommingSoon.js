110cd68522b938234b5b7bd7810ce66fd3f8540f
Marc Romo <marcromotarre@gmail.com>
2020-12-02 00:14:07 +0100

    vercel routes

--- src/components/coming-soon/CommingSoon.js
+++ src/components/coming-soon/CommingSoon.js
@@ -1,137 +1,79 @@
-import React, { useEffect, useState } from "react";
-import { ReactSVG } from "react-svg";
-import styled, { css } from "styled-components";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  useLocation
+} from "react-router-dom";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  flex-direction: column;
-`;
+// You can use the last <Route> in a <Switch> as a kind of
+// "fallback" route, to catch 404 errors.
+//
+// There are a few useful things to note about this example:
+//
+// - A <Switch> renders the first child <Route> that matches
+// - A <Redirect> may be used to redirect old URLs to new ones
+// - A <Route path="*> always matches
 
-const Proximamente = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  justify-content: center;
-  p {
-    font-family: Poppins-Thin;
-    padding: 0;
-    margin: 0;
-    font-size: 40px;
-  }
-  .grey {
-    color: #b5b5b5;
-  }
-  .grey-shadow {
-    color: #c4c4c4;
-  }
-
-  .red {
-    color: #ea4335;
-  }
-  .red-shadow {
-    color: #ec564a;
-  }
-
-  .yellow {
-    color: #fbf205;
-  }
-  .yellow-shadow {
-    color: #fcf31e;
-  }
-
-  .blue {
-    color: #4285f4;
-  }
-  .blue-shadow {
-    color: #5591f5;
-  }
-
-  .green {
-    color: #ea4335;
-  }
-  .green-shadow {
-    color: #49b165;
-  }
-
-  .black {
-    color: #000000;
-  }
-  .black-shadow {
-    color: #1a1a1a;
-  }
-`;
-
-function CommingSoon() {
-  const colors = [
-    { name: "red" },
-    { name: "blue" },
-    { name: "green" },
-    { name: "yellow" },
-    { name: "black" }
-  ];
-  var randomNumber = Math.floor(Math.random() * colors.length);
-  const svgName = `sevenmeeples${colors[randomNumber].name}.svg`;
-
-  const size = useWindowSize();
-
-  const meepleHeight = size.height * 0.6;
-  const meepleWidth = size.width * 0.6;
-  const aspect_ratio = 1.14;
-
+export default function NoMatchExample() {
   return (
-    <Container>
-      <ReactSVG
-        src={svgName}
-        aria-label="7 meeples"
-        role="img"
-        beforeInjection={svg => {
-          svg.setAttribute("style", `height: ${meepleHeight}`);
-          svg.setAttribute("style", `width: ${meepleHeight / aspect_ratio}`);
-        }}
-      />
-      <Proximamente>
-        <p className={"grey-shadow"}>PROX</p>
-        <p className={"grey"}>IM</p>
-        <p className={`${colors[randomNumber].name}-shadow`}>AME</p>
-        <p className={`${colors[randomNumber].name}`}>NTE</p>
-      </Proximamente>
-    </Container>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/old-match">Old Match, to be redirected</Link>
+          </li>
+          <li>
+            <Link to="/will-match">Will Match</Link>
+          </li>
+          <li>
+            <Link to="/will-not-match">Will Not Match</Link>
+          </li>
+          <li>
+            <Link to="/also/will/not/match">Also Will Not Match</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/old-match">
+            <Redirect to="/will-match" />
+          </Route>
+          <Route path="/will-match">
+            <WillMatch />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default CommingSoon;
+function Home() {
+  return <h3>Home</h3>;
+}
 
-// Hook
-function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined
-  });
+function WillMatch() {
+  return <h3>Matched!</h3>;
+}
 
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    }
+function NoMatch() {
+  let location = useLocation();
 
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-
-  return windowSize;
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
 }
